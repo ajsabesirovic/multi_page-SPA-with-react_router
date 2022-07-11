0e5616a91bf7b4f4bc7a88b90cbc4720904ba5a8
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-07-11 16:22:24 +0200

    adding the comments features

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,51 @@
-import { useState } from "react";
-
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
 import classes from "./Comments.module.css";
 import NewCommentForm from "./NewCommentForm";
-
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentsList from "../comments/CommentsList";
 const Comments = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedComments,
+  } = useHttp(getAllComments, true);
+
+  const params = useParams();
+  const { quoteId } = params;
+
   const [isAddingComment, setIsAddingComment] = useState(false);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  let comments;
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet!</p>;
+  }
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   return (
     <section className={classes.comments}>
@@ -18,8 +55,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
